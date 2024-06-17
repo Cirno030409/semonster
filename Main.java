2a4d716cbfe19a7f9d4c2d11f1bac6325ee6b71c
Tanimura Yuta <129668520+Cirno030409@users.noreply.github.com>
2024-06-17 13:00:41 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("SEMONSTER GAME");
     System.out.println("Show draw monsters!");
-    System.out.println("シーサーペント:レア度[1]");
+    System.out.println(monster.toString());
   }
 }
