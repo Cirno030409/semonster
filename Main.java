fe31856419985c11c14d253c6e66fab6aa99ae0c
Tanimura Yuta <129668520+Cirno030409@users.noreply.github.com>
2024-06-17 13:02:04 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,9 @@
-import java.util.Random;
-
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMONSTER GAME");
-    System.out.println("Show draw monsters!");
+    System.out.println("SEMonster!");
     Player player = new Player();
     player.drawMonsters();
     player.showDeck();
+
   }
 }
